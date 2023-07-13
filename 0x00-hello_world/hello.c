0f5607b71ab9a7fea4873859436e4f47e2160e94
1manasseh23 <cletavivan@gmail.com>
2023-07-13 15:56:53 -0700

    Preprocessor

---
+++ 0x00-hello_world/hello.c
@@ -0,0 +1,12 @@
+ #intclude < stdio.h >
+ /**
+ * main - entry point
+ * Description: This is to print out hello world on the screen
+ * return(0);
+ */
+
+ int main(void);
+  {
+	printf ("Hello World\n");
+	return (0);
+  }

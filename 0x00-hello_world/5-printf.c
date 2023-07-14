1d4ea15bcbfec956a24689d0e49be64795a55c69
1manasseh23 <cletavivan@gmail.com>
2023-07-13 18:18:45 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * maim - Entry point
- * Description: This is to print with proper grammar, but the outcome is a piece of art
- * Return: always 0 (success)
- */
+* maim - Entry point
+* Description: Print with proper grammar, but the outcome is a piece of art,
+* Return: always 0 (success)
+*/
 int main(void)
- {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
- }
+}

d8e1dbbe43bc86f42ac8ca3d89bf42b144696677
1manasseh23 <cletavivan@gmail.com>
2023-07-13 18:03:52 -0700

    Hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * maim - Entry poin
+ * Description:	This is to programming is like building a multilingual puzzle
+ * Return:	alway 0 (success);
+ */
+ int main(void)
+ {
+ 	puts("Programming is like building a multilingual puzzle");
+	return	(0);
+ }

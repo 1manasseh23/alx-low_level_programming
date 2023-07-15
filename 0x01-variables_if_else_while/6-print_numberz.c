9ad3fa3fbaae268b3cdd6f285ea6b7a5c0167b49
1manasseh23 <cletavivan@gmail.com>
2023-07-15 08:17:18 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,18 +3,18 @@
 #include <time.h>
 /**
  * main - Entry point
- * Description: This print number to 10 from 0.
+ * Description: This print number 10 from 0.
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-	int x;
+	int g;
 
-	for (x = 0; x < 10; x++)
+	for (g = 0; g < 10; g++)
 	{
-		printf("%d", x);
+		putchar(g);
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }

f6d7b319ed20f6754954987970719ed8d4cd142b
1manasseh23 <cletavivan@gmail.com>
2023-07-24 15:53:00 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
  /**
-  * swap_int - A function that swap the values of two integers.
-  * @a: an inout integer pointer
+  * swap_int - a function that swap the values of two integers.
+  * @a: an inoput integer pointer
   * @b: an input integers pointer
-  * Description: Void swap_int function swaps the values of two integers.
+  * Description: This function swaps the values of two integers.
   * Return: Nothing
  */
 void swap_int(int *a, int *b)

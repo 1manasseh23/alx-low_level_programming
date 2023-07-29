828750f9b1a8185f858c4f95b7475b49a699f480
1manasseh23 <cletavivan@gmail.com>
2023-07-29 07:35:16 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: 0 (success)
- */
+/*  main - Entry point */
 int main(void)
 {
 	int n;
-	int z[5];
-	int *b;
+	int a[5];
+	int *p;
 
-	z[2] = 1024;
-	b = &n;
+	a[2] = 1024;
+	p = &n;
 	/*
-	 * write your line of code
-	 * Remember: - you are not allow to use a
-	 * - you are not allow to use p, only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(z + 5) = 98;
-	/* ...so that this can prints 98\n */
-	printf("z[2] = %d\n", z[2]);
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }

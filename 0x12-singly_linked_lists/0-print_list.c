9898f035cf2845f8777777493c265b8da5a04489
1manasseh23 <cletavivan@gmail.com>
2023-08-24 21:14:31 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,7 @@ size_t print_list(const list_t *h)
 	{
 		printf("[%u] ", h->len);
 		if (h->str == NULL)
-			printf("(nil)\n");
-		else if (h->str == NULL)
-			printf("[0]");
+			printf("[0] (nil)\n");
 		else
 			printf("%s\n", h->str);
 		h = h->next;

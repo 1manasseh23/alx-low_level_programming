d0317f10087a047278ad50b3b35812ff0eb77b54
1manasseh23 <cletavivan@gmail.com>
2023-08-29 02:49:05 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,10 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	while (*head != NULL)
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	temp = *head;
+	while (*head)
 	{
-		listint_t *temp = *head;
-		*head = (*head)->next;
+		temp = *head;
+		(*head) = (*head)->next;
 		free(temp);
 	}
 	*head = NULL;

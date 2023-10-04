9bdd2efafac6d0285a3c760655bac1ea3033d229
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-04 06:34:22 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 
 	for (; i < height; i++)
 	{
-		gd[i] = malloc(width * sizeof(int));
+		gd[i] = (int *)malloc(width * sizeof(int));
 
 		if (gd[i] == NULL)
 		{

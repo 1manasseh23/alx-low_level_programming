bb2de8d6b6721e77b6de34496aceed49789f6a4d
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-10 14:44:31 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,17 @@
 #ifndef __DOG_H__
 #define __DOG_H__
 
+/**
+ * struct dog - a function Define a new type struct dog
+ * with the following elements
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: THe dog owner
+ * Description: A data structure for dog
+ */
 
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

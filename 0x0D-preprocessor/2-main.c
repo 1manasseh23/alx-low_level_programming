f3d1c6becfc53dd112ba05789b90ba0346f6ec95
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-10 04:22:55 -0700

    Object-like Macro, sum

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A function that Write a program that prints the name of the
+ * file it was compiled from, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf(__FILE__ "\n");
+	return (0);
+}

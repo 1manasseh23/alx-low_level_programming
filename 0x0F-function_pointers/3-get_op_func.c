03a014f2bee7d7cedc95556e0ea7699e99831b43
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-11 09:31:03 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - A function that selects the correct function to
+ * perform the operation asked by the user. You’re not allowed to
+ * declare any other function.
+ * @s: An input character
+ * Return: Character
+ */
+
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - A functio that add a and b
+ * @a: An input integer
+ * @b: An input integer
+ * Return: the sum a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - A functio that subtract a by b
+ * @a: An input integer
+ * @b: An input integer
+ * Return: the sum a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - A functio that multiply a by b
+ * @a: An input integer
+ * @b: An input integer
+ * Return: the sum a and b
+ */
+
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - A functio that divde a by b
+ * @a: An input integer
+ * @b: An input integer
+ * Return: the sum a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - A functio that returns the remainder of the division of a by b
+ * @a: An input integer
+ * @b: An input integer
+ * Return: the sum a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

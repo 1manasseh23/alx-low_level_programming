6d2e8e4f9f7f4396729bf71ad86421ae2caf88cf
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-25 03:00:55 -0700

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,14 +16,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *cnode;
 
 	if (*head == NULL)
-		return (1);
+		return (-1);
 
 	if (index == 0)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
-		return (1);
+		return (-1);
 	}
 	else
 	{
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			if (temp->next != NULL)
 			{
-				return (-1);
+				return (1);
 			}
 			temp = temp->next;
 		}

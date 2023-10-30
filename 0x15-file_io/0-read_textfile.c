88774757e71035eeff84ed70e63b1e62cd257090
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-30 07:30:06 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,19 +17,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		dprintf(2, "\n");
 		return (0);
 	}
 	file_print = open(filename, O_RDONLY);
 	if (file_print == -1)
 	{
-		dprintf(2, "\n");
 		return (0);
 	}
 	buffer = malloc(letters * sizeof(char));
 	if (buffer == NULL)
 	{
-		dprintf(2, "\n");
 		close(file_print);
 		return (0);
 	}
@@ -43,7 +40,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		dprintf(2, "\n");
 		free(buffer);
 		close(file_print);
 		return (0);

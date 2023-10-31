962f965bd1ed7b5f0bd5318f8ff423053d7b1bd2
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-10-30 17:47:40 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+int main(int argc, char *argv[]);
 /**
  * main - A function that copies the content of a file to another file
  * @argc: Number of argument to passed
@@ -26,7 +28,8 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IRGRP | S_IROTH);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IRGRP | S_IROTH);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);

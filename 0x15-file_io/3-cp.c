793e5c1a6c820d45d3b75b93aa5ad3b40df3ebaa
1manasseh23 <you@example.comcletavivan@gmail.com>
2023-11-02 17:49:39 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IRGRP | S_IROTH);
+			S_IRUSR | S_IRGRP | S_IWUSR | S_IROTH);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
